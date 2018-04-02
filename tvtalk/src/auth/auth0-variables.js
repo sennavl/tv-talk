ec7eab781a03f4f724aebfbbdaebe103fb1f5b62
senna.vanlondersele <senna.vanlondersele@student.odisee.be>
2018-04-02 14:29:23 +0200

    Added login and register with Auth0

---
+++ tvtalk/src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+	clientId: 'n6_erpXmB2u71mapz6lg75649CXPXYVZ',
+	domain: 'movies.eu.auth0.com',
+	callbackUrl: 'http://localhost:8080/callback',
+	apiUrl: 'API_IDENTIFIER'
+}
