4a48ee5648e491883ce69d2e03d07b23ad00bafe
senna.vanlondersele <senna.vanlondersele@student.odisee.be>
2018-04-02 19:29:33 +0200

    Added styling + basic api functionality

--- tvtalk/src/App.vue
+++ tvtalk/src/App.vue
@@ -1,5 +1,100 @@
 <template>
-  <div>
+  <div id="wrapper">
+
+	  <!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Phantom</span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="generic.html">Ipsum veroeros</a></li>
+							<li><a href="generic.html">Tempus etiam</a></li>
+							<li><a href="generic.html">Consequat dolor</a></li>
+							<li><a href="elements.html">Elements</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<h1>This is Phantom, a free, fully responsive site<br />
+								template designed by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+								<p>Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.</p>
+							</header>
+							<section class="tiles" v-if="popularMovies && popularMovies.length">
+								<article v-for="movie of popularMovies" :key="movie.id">
+									<span class="image">
+										<img :src=baseUrlPoster+movie.poster_path alt="" />
+									</span>
+									<a>
+										<h2>{{movie.title}}</h2>
+										<div class="content">
+											<p>{{movie.overview.substring(0, 75) + "..."}}</p>
+										</div>
+									</a>
+								</article>
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Get in touch</h2>
+								<form method="post" action="#">
+									<div class="field half first">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="special" /></li>
+									</ul>
+								</form>
+							</section>
+							<section>
+								<h2>Follow</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+									<li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -28,6 +123,12 @@
       </div>
     </nav>
 
+	<ul v-if="errors && errors.length">
+		<li v-for="error of errors" :key="error.id">
+		{{error.message}}
+		</li>
+	</ul>
+
     <div class="container">
       <router-view
         :auth="auth"
@@ -38,26 +139,49 @@
 </template>
 
 <script>
-import AuthService from './auth/AuthService'
+import AuthService from "./auth/AuthService";
+import axios from "axios";
 
-const auth = new AuthService()
+const auth = new AuthService();
 
-const { login, logout, authenticated, authNotifier } = auth
+const { login, logout, authenticated, authNotifier } = auth;
 
 export default {
-  name: 'app',
-  data () {
-    authNotifier.on('authChange', authState => {
-      this.authenticated = authState.authenticated
-    })
+  name: "app",
+  data() {
+    authNotifier.on("authChange", authState => {
+      this.authenticated = authState.authenticated;
+    });
     return {
       auth,
-      authenticated
-    }
+      authenticated,
+      popularMovies: [],
+      errors: [],
+      baseUrlPoster: "https://image.tmdb.org/t/p/original"
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=09767dbf40d373b1e78aa80db4deefc9&language=en-US&page=1"
+      )
+      .then(response => {
+        console.log(response.data.results);
+        this.popularMovies = response.data.results.splice(0, 3);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   },
   methods: {
     login,
     logout
   }
-}
+};
 </script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+</style>
--- tvtalk/src/router/index.js
+++ tvtalk/src/router/index.js
@@ -21,7 +21,7 @@ const router = new Router({
     {
       path: '*',
       redirect: '/home'
-    }
+	}
   ]
 })
 
