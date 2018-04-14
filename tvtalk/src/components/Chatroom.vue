0b9a4e2c768529264970a8893ca076380f4552fc
senna.vanlondersele <senna.vanlondersele@student.odisee.be>
2018-04-14 14:15:00 +0200

    Added basic chat implementation with socketio

--- tvtalk/src/components/Chatroom.vue
+++ tvtalk/src/components/Chatroom.vue
@@ -1,22 +1,37 @@
 <template>
   <div>
-
+	  <div id="chat">
+            <h2>Chat</h2>
+            <div id="chat-window">
+                <div id="output"></div>
+            </div>
+            <input id="handle" type="text" placeholder="Handle" />
+            <input id="message" type="text" placeholder="Message" />
+            <button id="send" @click.prevent="sendMessage">Send</button>
+        </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-var socket = io.connect();
+const io = require('socket.io-client');
+var socket = io.connect('http://localhost:4113');
 
-socket.on('stream', function(data){
-app.title = data.title;
-});
+export default {
 
-export default{
-	data() {
-		return{
-			title: 'ugly title'
-		}
-	},
+  data() {
+    return {
+      isConnected: false,
+      socketMessage: ''
+    }
+  },
+  methods: {
+	  sendMessage() {
+		  socket.emit('chat', {
+			  message: 'hallo',
+			  handle: 'senna'
+		  })
+	  }
+  }
 }
 </script>
 
